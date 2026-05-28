d024bb67421b7fee5e697b1b24958bdb244c8452
Roman Deulio <roman@deulio.fr>
2026-05-28 11:18:11 +0200

    fix(asgi): call django.setup() before importing app modules
    asgi.py was importing realtime.middleware before Django was configured,
    which made daphne crash on boot with:
      ImproperlyConfigured: Requested setting INSTALLED_APPS, but settings
      are not configured
    
    Set DJANGO_SETTINGS_MODULE + django.setup() first, then import middleware
    and routing.

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -1,11 +1,15 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from realtime.middleware import JWTAuthMiddleware
-from realtime.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django.setup()
+
 django_asgi_app = get_asgi_application()
+
+from realtime.middleware import JWTAuthMiddleware
+from realtime.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
