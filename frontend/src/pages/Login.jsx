9a42f46ba713b2c38c4dcf386c279743c24972ff
LuThaTha <thais.vandooren@outlook.com>
2026-06-01 13:34:29 +0200

    Fix: button auth42

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,9 +7,11 @@ export default function Login() {
         <div className={styles.logo}>⚽</div>
         <h1 className={styles.title}>BABYFOOT 42</h1>
         <p className={styles.sub}>Connecte-toi pour accéder à la plateforme</p>
+        <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-29dcfe906e0f8b18e2511684727174672ce9648b697ddb278b04095f22bdebae&redirect_uri=https%3A%2F%2Flocalhost%2Fprofil&response_type=code">
         <button className={styles.btn42}>
           Se connecter avec 42
         </button>
+        </a>
         <div className={styles.divider}>ou</div>
         <input className={styles.input} placeholder="Email" type="email" />
         <input className={styles.input} placeholder="Mot de passe" type="password" />
